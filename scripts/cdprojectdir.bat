8f66459eb6b31d06aef9aa78586fdea4fc4e47ba
twigs <pfaffflo@gmx.de>
2012-03-01 13:33:32 +0100

    - added cdprojectdir.bat and setprojectdir.bat  - updated setup.py  - updated README.rst

---
+++ scripts/cdprojectdir.bat
@@ -0,0 +1,35 @@
+@echo off
+
+goto CHANGEDIR
+:USAGE
+	echo.
+	echo  switches to the project dir of the activated virtualenv
+	echo.
+	goto END
+
+:CHANGEDIR
+
+	if not defined VIRTUAL_ENV (
+	   echo .
+	   echo a virtualenv need to be activated
+	   goto USAGE
+	)
+
+	if not defined VIRTUALENVWRAPPER_PROJECT_FILENAME (
+			set VIRTUALENVWRAPPER_PROJECT_FILENAME=.project
+		)
+
+	set _LAST_DIR=%CD%
+
+		
+	if not EXIST "%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%" (
+		echo.
+		echo No project directory found for current virtualenv
+		goto USAGE
+	)
+
+		
+		set /p ENVPRJDIR=<%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%
+		cd /d "%ENVPRJDIR%"
+
+:END
---
+++ scripts/setprojectdir.bat
@@ -0,0 +1,48 @@
+@echo off
+
+if [%1]==[] goto USAGE
+goto SETPROJECTDIR
+
+
+:USAGE
+	echo.
+	echo pass a full path to project directory while target virtualenv is activated
+	echo.
+	goto END
+
+
+:SETPROJECTDIR
+
+
+	if not defined WORKON_HOME (
+		set WORKON_HOME=%USERPROFILE%\Envs
+	)
+	
+	if not defined VIRTUALENVWRAPPER_PROJECT_FILENAME (
+		set VIRTUALENVWRAPPER_PROJECT_FILENAME=.project
+	)
+
+	if not defined VIRTUAL_ENV (
+		echo.
+		echo No novirtualenv activated.
+		goto USAGE
+		
+	) else (
+	
+		pushd %1 2>NUL && popd
+		@if errorlevel 1 (
+			mkdir "%1"
+		)
+		echo PRJDIR = "%1"
+		echo PRJfile = "%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%"
+		set /p =%1>%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME% <NUL
+		call add2virtualenv.bat %1
+		
+	)
+
+
+	
+
+
+
+:END
