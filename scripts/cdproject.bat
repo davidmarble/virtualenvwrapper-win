2820ac33c21e82ea538c74b9f0e65442699df5e1
David Marble <davidmarble@gmail.com>
2013-03-04 14:34:58 -0800

    Missed some tabs to spaces

--- scripts/cdproject.bat
+++ scripts/cdproject.bat
@@ -2,34 +2,34 @@
 
 goto CHANGEDIR
 :USAGE
-	echo.
-	echo  switches to the project dir of the activated virtualenv
-	echo.
-	goto END
+    echo.
+    echo  switches to the project dir of the activated virtualenv
+    echo.
+    goto END
 
 :CHANGEDIR
 
-	if not defined VIRTUAL_ENV (
-	   echo .
-	   echo a virtualenv must be activated
-	   goto USAGE
-	)
+    if not defined VIRTUAL_ENV (
+       echo .
+       echo a virtualenv must be activated
+       goto USAGE
+    )
 
-	if not defined VIRTUALENVWRAPPER_PROJECT_FILENAME (
-			set VIRTUALENVWRAPPER_PROJECT_FILENAME=.project
-		)
+    if not defined VIRTUALENVWRAPPER_PROJECT_FILENAME (
+        set VIRTUALENVWRAPPER_PROJECT_FILENAME=.project
+    )
 
-	set _LAST_DIR=%CD%
+    set _LAST_DIR=%CD%
 
-		
-	if not EXIST "%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%" (
-		echo.
-		echo No project directory found for current virtualenv
-		goto USAGE
-	)
+        
+    if not EXIST "%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%" (
+        echo.
+        echo No project directory found for current virtualenv
+        goto USAGE
+    )
 
-		
-		set /p ENVPRJDIR=<"%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%"
-		cd /d "%ENVPRJDIR%"
+        
+    set /p ENVPRJDIR=<"%VIRTUAL_ENV%\%VIRTUALENVWRAPPER_PROJECT_FILENAME%"
+    cd /d "%ENVPRJDIR%"
 
 :END
