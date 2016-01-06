f59206a635fc8ae363f1aa49fdd6c8b96be35b04
thehug0naut <thehug0naut@fastmail.fm>
2016-01-06 13:34:51 +0000

    Added mkproject batch script to implement

---
+++ scripts/mkproject.bat
@@ -0,0 +1,53 @@
+@echo off
+
+if [%1]==[] goto USAGE
+goto MKVIRTUALENV
+
+
+:USAGE
+echo.
+echo.    Pass a name to create a new virtualenv and linked project
+goto END
+
+
+:LINKPROJECT
+call setprojectdir.bat "%PROJ%"
+call cdproject.bat
+goto END
+
+
+:MKPROJECT
+if not defined PROJECT_HOME (
+    set "PROJECT_HOME=%USERPROFILE%\.projects"
+)
+
+pushd %PROJECT_HOME% 2>NUL && popd
+if errorlevel 1 (
+    mkdir %PROJECT_HOME%
+)
+
+set "PROJ=%PROJECT_HOME%\%1"
+
+pushd %PROJ% 2>NUL && popd
+if errorlevel 1 (
+    mkdir %PROJ%
+    goto LINKPROJECT
+) else (
+    echo.
+    echo.    project "%PROJ%" already exists
+    goto END
+)
+
+
+:MKVIRTUALENV
+call mkvirtualenv.bat %1
+
+if not defined VIRTUAL_ENV (
+    goto END
+)
+
+goto MKPROJECT
+
+
+:END
+set PROJ=
