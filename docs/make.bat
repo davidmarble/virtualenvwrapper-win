4937342ddbdb829d19c409dbeb3a13420e460186
thebjorn <bp@datakortet.no>
2017-11-08 21:58:41 +0100

    Building the docs to the /build/docs directory.

--- docs/make.bat
+++ docs/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=.
-set BUILDDIR=_build
+set BUILDDIR=../build/docs
 set SPHINXPROJ=virtualenvwrapper-win
 
 if "%1" == "" goto help
